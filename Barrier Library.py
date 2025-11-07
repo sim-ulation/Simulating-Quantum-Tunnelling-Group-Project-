49f9bdf69f8bdc7a8c85662e8d53f317e64feadd
sim-ulation <sim.dach75@gmail.com>
2025-11-07 10:04:08 +0000

    Barrier Library.py
    Triangle Barrier: shows smooth, gradual potential effect on transmission - less reflection than sharp rectangular barrier because the wave has time to 'adjust' to the changing potential.
    
    Gaussian Barrier: Smooth bell-curve shape. Most physically realistic (actual potentials in nature ex. atomic interactions). Shows cleanest tunnelling behaviour because its infinitely smooth.
    
    Double Rectangular Barrier: Demonstrates resonant tunnelling. If wave energy matches the resonance values of the well between barriers, transmission increases dramatically.

--- Barrier Library.py
+++ Barrier Library.py
@@ -1,29 +1,33 @@
 # -*- coding: utf-8 -*-
-"""
+"""-------------------------------------------------------------
 Barrier Library
-"""
+-------------------------------------------------------------"""
 
 #Triangle
 V = 0*x
 for i in range(len(V)):
-    if x[i] > -0.5 and x[i] < 0.25:
+    if x[i] > -0.5 and x[i] < 0.25: #linear ramp up; peak at 0.25
         
         V[i] = V0 * (x[i] - (-0.5)) / (0.25 - (-0.5))
-    elif x[i] >= 0.25 and x[i] < 1.0:
+    elif x[i] >= 0.25 and x[i] < 1.0: #linear ramp down; peak at o.25
         
         V[i] = V0 * (1.0 - x[i]) / (1.0 - 0.25)
-"""
-starts at x=-0.5 with V=0, linearly increases to V=V0 at x=0.25 (peak), 
-linearly decreases back to V=0 at x=1.0. total width =1.5 units, peak at 0.25
-
-    -> shows how smooth, gradual potential affects transmission. less
-    reflection than sharp rectangular barrier because the wave has time to
-    "adjust" to the changing potential
-"""
 
 #Gaussian
 V = 0*x
-centre = 0.25
-width = 0.3
+centre = 0.25 #centre position; change this to change the position
+width = 0.3 #width
 for i in range(len(V)):
+
     V[i] = V0 * np.exp(-(x[i] - centre)**2 / (2*width**2))
+
+#Rectangle 
+for i in range(len(V)):
+    if x[i]>0 and x[i]<1: #width of well
+        V[i]=V0
+
+#Double Rectangle
+V = 0*x
+for i in range(len(V)):
+    if (x[i] > -0.8 and x[i] < -0.3) or (x[i] > 0.3 and x[i] < 0.8): # (first barrier) or (second barrier). 
+        V[i] = V0
